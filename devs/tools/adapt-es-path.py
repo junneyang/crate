b90690363f3b09e4cc99bd89e90f646ac5268f77
Mathias Fussenegger <f.mathias@zignar.net>
2020-05-07 15:35:28 +0200

    Integrate es-server into server

--- devs/tools/adapt-es-path.py
+++ devs/tools/adapt-es-path.py
@@ -14,9 +14,6 @@ def main():
     for line in sys.stdin:
         sys.stdout.write(
             line
-            .replace('diff --git a/server/', 'diff --git a/es/es-server/')
-            .replace('--- a/server/', '--- a/es/es-server/')
-            .replace('+++ b/server/', '+++ b/es/es-server/')
             .replace('diff --git a/test/framework', 'diff --git a/es/es-testing/')
             .replace('--- a/test/framework', '--- a/es/es-testing/')
             .replace('+++ b/test/framework', '+++ b/es/es-testing/')
